867b6cbb405875b1ec0d2dc8a10a831df99fd21b
Pablo <Pablo@Pablos-MacBook-Pro.local>
2019-04-22 20:33:41 -0700

    Add urls index template

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var PORT = 8080;
 
-var handlers = require("./handlers");
+var handler = require("./handlers");
 
 app.set("view engine", "ejs");
 
@@ -16,9 +16,9 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
 
-app.get("/hello", handlers.helloWorld);
+app.get("/hello", handler.helloWorld);
 
-app.get("/urls", handlers.urls);
+app.get("/urls", handler.urls);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
